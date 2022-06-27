5973297b323e7e4aa2b1aa5cccc20eff5aa8097d
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-27 19:53:07 +0200

    renmoved test component from index page

--- components/cards/MatchUp.Card.tsx
+++ components/cards/MatchUp.Card.tsx
@@ -102,3 +102,23 @@ const MatchUpCard: React.FunctionComponent<IMatchUpCardProps> = (props) => {
 };
 
 export default MatchUpCard;
+
+/* 
+
+USAGE:
+
+<MatchUpCard
+  timestamp={'August 24th'}
+  title={'Arabas vs. Italianas with shorts'}
+  slots={10}
+  participating={2}
+  location={'Treptower Park, Berlin'}
+  sport={'basketball'}
+  skill={'beginner'}
+  imageUrl={footballPic}
+  paid={true}
+  price={10}
+  rented={true}
+></MatchUpCard>
+
+*/
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,7 @@
 import type { NextPage } from 'next';
-import MatchUpCard from '../components/cards/MatchUp.Card';
-import footballPic from '../public/football.jpg';
 
 const Home: NextPage = () => {
-  return (
-    <MatchUpCard
-      timestamp={'August 24th'}
-      title={'Arabas vs. Italianas with shorts'}
-      slots={10}
-      participating={2}
-      location={'Treptower Park, Berlin'}
-      sport={'basketball'}
-      skill={'beginner'}
-      imageUrl={footballPic}
-      paid={true}
-      price={10}
-      rented={true}
-    ></MatchUpCard>
+  return (<></>
   );
 };
 
