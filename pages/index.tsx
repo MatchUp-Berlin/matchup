f1a19d077db5d72fa2384ff1555d11c60af14f34
Mitchell <mitchellryansmith@gmail.com>
2022-06-28 16:31:41 +0200

    added navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,18 @@
 import type { NextPage } from 'next';
-<<<<<<< HEAD
-import { usersArray } from '../mockData/mockUsersArray';
-import { useTheme } from '../contexts/Theme';
-
-const Home: NextPage = () => {
-  const { toggleDarkMode } = useTheme();
-  return (
-    <div>
-      <h1>🐋 Blanalala 🐋</h1>
-      <button onClick={toggleDarkMode}> darkMode</button>
-    </div>
-=======
 import MatchUpCard from '../components/cards/MatchUp.Card';
 import football from '../public/football.jpg';
+import Navigation from '../components/misc/Navigation';
 
 const Home: NextPage = () => {
   return (
-    <MatchUpCard
+    <><MatchUpCard
       variant="small"
       timestamp="24 August"
       title="Arabs frommage"
       location="Treptower Park, Berlin"
       sport="football"
       imageUrl={football}
-    ></MatchUpCard>
->>>>>>> main
+    ></MatchUpCard><Navigation /></>
   );
 };
 
