767a4bcc2a4741ef31f6ffb952f7ef7f9c6fb1df
Mitchell <mitchellryansmith@gmail.com>
2022-06-28 11:33:18 +0200

    first test working

--- test/backend/getUserById.test.ts
+++ test/backend/getUserById.test.ts
@@ -1,6 +1,11 @@
 import { getUserById } from '../../utils/Query/getUserById.util'
 import { getUserByIdMock } from '../mocks/getUserById.mock'
 
+import Amplify from 'aws-amplify';
+import awsconfig from '../../src/aws-exports';
+
+Amplify.configure(awsconfig);
+
 test('get User by ID Appsync', async () => {
   const data = await getUserById('ceb7d85e-021e-4657-90a0-e3e2f98bcc7c');
   expect(data).toBe(getUserByIdMock);
