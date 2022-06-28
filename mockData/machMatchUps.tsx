4e7b93f2cab752ca1a5e6bfe913650c744eaebb4
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-28 20:24:27 +0200

    fixed positioning of sports filter

---
+++ mockData/machMatchUps.tsx
@@ -0,0 +1,22 @@
+import { MatchUp } from '../utils/types/MatchUp.Type';
+
+const matchUps: MatchUp[] = [
+  {
+    attendanceMax: 10,
+    attendanceMin: 2,
+    cancelled: false,
+    currency: 'USD',
+    date: '2022-06-27T15:33:52.444Z',
+    description: 'its sportin time',
+    id: '1367ec81-1fb2-449a-b97e-faa879d1a36c',
+    image: 'https://imageshare/image1',
+    location: 'berlin',
+    organizer: '54955977-c461-4943-9e66-288b3ba065ba',
+    reservedCourt: false,
+    skillLevel: 'beginner',
+    sportCategory: 'sportball',
+    title: 'sportin at the park',
+    totalCost: 0,
+    users: [],
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,26 @@
 import type { NextPage } from 'next';
 import { useTheme } from '../contexts/Theme';
 import Navigation from '../components/misc/Navigation';
+import { useState } from 'react';
+import Filter from '../components/misc/Filter';
+import SportFilter from '../components/misc/SportFilter';
+import styles from './styles/Explore.module.scss';
 
 const Home: NextPage = () => {
-  const { toggleDarkMode } = useTheme();
+  const { colors, toggleDarkMode } = useTheme();
+  const [categories, setCategories] = useState<Array<string>>([]);
+  const [timeFrame, setTimeFrame] = useState<{ from: string; to: string }>({
+    from: new Date().toISOString(),
+    to: new Date().toISOString(),
+  });
+  const [city, setCity] = useState<string>('Berlin');
   return (
-    <>
+    <div style={{ backgroundColor: colors.background[100] }} className={styles.page}>
+      <Filter city={city} setCity={setCity} setTimeFrame={setTimeFrame}></Filter>
+      <SportFilter categories={categories} setCategories={setCategories} />
       <button onClick={toggleDarkMode}>DarkMode</button>
       <Navigation />
-    </>
+    </div>
   );
 };
 
