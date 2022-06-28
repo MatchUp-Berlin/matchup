1954b3d89a54379026e39992abb180ce52953b0a
Samuel Fanhais <samuelfanhais@gmail.com>
2022-06-28 15:41:43 +0200

    feat: added updates preview button

--- components/cards/UpdatesPreview.Card.tsx
+++ components/cards/UpdatesPreview.Card.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useTheme } from '../../contexts/Theme';
+import styles from './styles/UpdatesPreview.Card.module.scss';
 
 export interface IUpdatesPreviewCardProps {}
 
 const UpdatesPreviewCard: React.FunctionComponent<IUpdatesPreviewCardProps> = (props) => {
-  return <></>;
+  const { colors } = useTheme();
+  return (
+    <>
+      <h1>Something else</h1>
+    </>
+  );
 };
 
 export default UpdatesPreviewCard;
