3af6e3e7ae8378d0cfeb0f5ad8a22b230804be8f
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-28 16:01:24 +0200

    created sports filter

--- components/misc/SportFilterButton.tsx
+++ components/misc/SportFilterButton.tsx
@@ -1,21 +1,30 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import { useTheme } from '../../contexts/Theme';
 import styles from './styles/SportFilterButton.module.scss';
 
 export interface ISportFilterButtonProps {
+  setCategories: Dispatch<SetStateAction<string[]>>;
   icon: any;
-  active: boolean;
+  active?: boolean;
+  category: 'basketball' | 'football' | 'tennis' | 'ultimate-frisbee' | 'beach-volleyball' | 'volleyball';
 }
 
 const SportFilterButton: React.FunctionComponent<ISportFilterButtonProps> = (props) => {
   const { shadows, colors } = useTheme();
   return (
     <div
+      onClick={() =>
+        props.active
+          ? props.setCategories((prev) => prev.filter((cat) => cat !== props.category))
+          : props.setCategories((prev) => [...prev, props.category])
+      }
       className={styles.wrapper}
       style={{
         boxShadow: shadows.small,
         backgroundColor: props.active ? colors.background[60] : colors.background[80],
+        outlineColor: colors.primary[100],
+        outlineWidth: props.active ? 4 : 0,
       }}
     >
       <Image src={props.icon} alt="sports-filter-button" width="35px" height="35px"></Image>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
+import SportFilter from '../components/misc/SportFilter';
 import { useTheme } from '../contexts/Theme';
 
 const Home: NextPage = () => {
   const { toggleDarkMode } = useTheme();
+  const [categories, setCategories] = useState<Array<string>>([]);
   return (
     <>
+      <SportFilter categories={categories} setCategories={setCategories} />
       <button onClick={toggleDarkMode}>DarkMode</button>
     </>
   );
