c6c766f3a7f9f9217820c9ffc38b53406bb0eeee
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-28 19:30:46 +0200

    feat: almost finished with filtergroup thingy

--- components/misc/Filter.tsx
+++ components/misc/Filter.tsx
@@ -1,41 +1,103 @@
-import React, { LegacyRef, useRef } from 'react';
+import React, { Dispatch, LegacyRef, SetStateAction, useRef, useState } from 'react';
 import { useTheme } from '../../contexts/Theme';
 import styles from './styles/Filter.module.scss';
 
-export interface IFilterProps {}
+export interface IFilterProps {
+  setTimeFrame: Dispatch<SetStateAction<{ from: string; to: string }>>;
+  setCity: Dispatch<SetStateAction<string>>;
+  city: string;
+}
 
 const Filter: React.FunctionComponent<IFilterProps> = (props) => {
-  const datePickerRef = useRef<LegacyRef<HTMLInputElement>>();
   const { colors, darkMode } = useTheme();
 
-  function openDateTimePicker(e: any) {
-    e.preventDefault();
-    if ('showPicker' in HTMLInputElement.prototype) {
-      //@ts-ignore
-      datePickerRef.current.type = 'datetime-local';
-      //@ts-ignore
-      datePickerRef.current.showPicker();
-    } else {
+  function getNextDayOfTheWeek(dayName: string, excludeToday = true, refDate = new Date()) {
+    const dayOfWeek = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'].indexOf(dayName);
+    refDate.setHours(0, 0, 0, 0);
+    refDate.setDate(
+      refDate.getDate() + +!!excludeToday + ((dayOfWeek + 7 - refDate.getDay() - +!!excludeToday) % 7)
+    );
+    return refDate;
+  }
 
+  function handleTimeFrameChange(e: any) {
+    switch (e.target.value) {
+      case '0': {
+        const start = new Date();
+        start.setUTCHours(0, 0, 0, 0);
+
+        const end = new Date();
+        end.setHours(23, 59, 59, 999);
+
+        props.setTimeFrame({
+          from: start.toISOString(),
+          to: end.toISOString(),
+        });
+        break;
+      }
+      case '1': {
+        const start = getNextDayOfTheWeek('fri');
+        const end = getNextDayOfTheWeek('sun');
+        end.setHours(23, 59, 59, 999);
+
+        props.setTimeFrame({
+          from: start.toISOString(),
+          to: end.toISOString(),
+        });
+        break;
+      }
+      case '2': {
+        // ACHTUNG BUG!!!
+        const start = getNextDayOfTheWeek('mon');
+        const end = getNextDayOfTheWeek('sun', false, start);
+        end.setHours(23, 59, 59, 999);
+
+        props.setTimeFrame({
+          from: start.toISOString(),
+          to: end.toISOString(),
+        });
+        break;
+      }
     }
   }
 
   return (
     <div className={styles.wrapper}>
       <input
-        style={{ borderColor: darkMode ? colors.background[80] : '#DDDDDD' }}
+        style={{ borderColor: darkMode ? colors.background[80] : '#DDDDDD', color: '#757575' }}
+        value={props.city}
+        onChange={(e) => props.setCity(e.target.value)}
         placeholder="Search for a city"
         className={styles.input + ' ' + styles.location}
       ></input>
-      <input
-        style={{ borderColor: darkMode ? colors.background[80] : '#DDDDDD' }}
-        ref={datePickerRef}
-        onClick={openDateTimePicker}
+      <select
+        style={{ borderColor: darkMode ? colors.background[80] : '#DDDDDD', color: '#757575' }}
         placeholder="Select time"
         className={styles.input + ' ' + styles.date}
-      ></input>
+        onChange={handleTimeFrameChange}
+      >
+        <option value="0">Today</option>
+        <option value="1">This Weekend</option>
+        <option value="2">Next Week</option>
+      </select>
     </div>
   );
 };
 
 export default Filter;
+
+/* 
+
+USAGE:
+
+const [categories, setCategories] = useState<Array<string>>([]);
+const [timeFrame, setTimeFrame] = useState<{ from: string; to: string }>({
+  from: new Date().toISOString(),
+  to: new Date().toISOString(),
+});
+const [city, setCity] = useState<string>('Berlin');
+
+<Filter city={city} setCity={setCity} setTimeFrame={setTimeFrame}></Filter>
+<SportFilter categories={categories} setCategories={setCategories} />
+
+*/
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,10 @@
 import type { NextPage } from 'next';
-import { useState } from 'react';
-import Filter from '../components/misc/Filter';
-import SportFilter from '../components/misc/SportFilter';
 import { useTheme } from '../contexts/Theme';
 
 const Home: NextPage = () => {
   const { toggleDarkMode } = useTheme();
-  const [categories, setCategories] = useState<Array<string>>([]);
   return (
     <>
-      <Filter></Filter>
-      <SportFilter categories={categories} setCategories={setCategories} />
       <button onClick={toggleDarkMode}>DarkMode</button>
     </>
   );
