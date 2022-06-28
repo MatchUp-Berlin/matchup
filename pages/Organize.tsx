b9c7062e801c315f832f49f38d9b7e88a24298e5
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-28 08:49:45 +0200

    bug: fixed darkmode on second organize page

--- pages/Organize.tsx
+++ pages/Organize.tsx
@@ -66,140 +66,149 @@ const OrganizePage: NextPage = () => {
   const [description, setDescription] = useState<string>('');
   const [image, setImage] = useState<File>();
 
-  if (step == 0)
-    return (
-      <div style={{ backgroundColor: colors.background[100] }}>
-        <Header
-          title="Choose a sport you want to play"
-          leftButton={
-            <HeaderButton
-              viewBox="0 0 10 10"
-              callback={() => Router.back()}
-              icon={
-                <path
-                  xmlns="http://www.w3.org/2000/svg"
-                  d="M5.96126 5L8.89307 2.075C9.02146 1.94661 9.09359 1.77248 9.09359 1.59091C9.09359 1.40934 9.02146 1.23521 8.89307 1.10682C8.76468 0.978432 8.59055 0.906303 8.40898 0.906303C8.22741 0.906303 8.05328 0.978432 7.92489 1.10682L4.99989 4.03864L2.07489 1.10682C1.9465 0.978432 1.77237 0.906303 1.5908 0.906303C1.40923 0.906303 1.2351 0.978432 1.10671 1.10682C0.978321 1.23521 0.906193 1.40934 0.906193 1.59091C0.906193 1.77248 0.978321 1.94661 1.10671 2.075L4.03853 5L1.10671 7.925C1.0428 7.98839 0.992081 8.0638 0.957466 8.14688C0.922851 8.22997 0.905029 8.31908 0.905029 8.40909C0.905029 8.4991 0.922851 8.58822 0.957466 8.6713C0.992081 8.75439 1.0428 8.8298 1.10671 8.89318C1.17009 8.95709 1.2455 9.00781 1.32859 9.04243C1.41168 9.07704 1.50079 9.09486 1.5908 9.09486C1.68081 9.09486 1.76993 9.07704 1.85301 9.04243C1.9361 9.00781 2.01151 8.95709 2.07489 8.89318L4.99989 5.96137L7.92489 8.89318C7.98828 8.95709 8.06369 9.00781 8.14677 9.04243C8.22986 9.07704 8.31897 9.09486 8.40898 9.09486C8.49899 9.09486 8.58811 9.07704 8.67119 9.04243C8.75428 9.00781 8.82969 8.95709 8.89307 8.89318C8.95698 8.8298 9.0077 8.75439 9.04232 8.6713C9.07693 8.58822 9.09475 8.4991 9.09475 8.40909C9.09475 8.31908 9.07693 8.22997 9.04232 8.14688C9.0077 8.0638 8.95698 7.98839 8.89307 7.925L5.96126 5Z"
-                />
-              }
-            />
-          }
-        ></Header>
-        <div className={styles.sportCategoriesList}>
-          <SportCard
-            title="Football (Soccer)"
-            subTitle="Start a football match with locals"
-            image={football}
-            callback={() => setSportCategory('football')}
-          ></SportCard>
+  return (
+    <div className={styles.wrapper} style={{ backgroundColor: colors.background[100] }}>
+      {step == 0 ? (
+        <>
+          <Header
+            title="Choose a sport you want to play"
+            leftButton={
+              <HeaderButton
+                viewBox="0 0 10 10"
+                callback={() => Router.back()}
+                icon={
+                  <path
+                    xmlns="http://www.w3.org/2000/svg"
+                    d="M5.96126 5L8.89307 2.075C9.02146 1.94661 9.09359 1.77248 9.09359 1.59091C9.09359 1.40934 9.02146 1.23521 8.89307 1.10682C8.76468 0.978432 8.59055 0.906303 8.40898 0.906303C8.22741 0.906303 8.05328 0.978432 7.92489 1.10682L4.99989 4.03864L2.07489 1.10682C1.9465 0.978432 1.77237 0.906303 1.5908 0.906303C1.40923 0.906303 1.2351 0.978432 1.10671 1.10682C0.978321 1.23521 0.906193 1.40934 0.906193 1.59091C0.906193 1.77248 0.978321 1.94661 1.10671 2.075L4.03853 5L1.10671 7.925C1.0428 7.98839 0.992081 8.0638 0.957466 8.14688C0.922851 8.22997 0.905029 8.31908 0.905029 8.40909C0.905029 8.4991 0.922851 8.58822 0.957466 8.6713C0.992081 8.75439 1.0428 8.8298 1.10671 8.89318C1.17009 8.95709 1.2455 9.00781 1.32859 9.04243C1.41168 9.07704 1.50079 9.09486 1.5908 9.09486C1.68081 9.09486 1.76993 9.07704 1.85301 9.04243C1.9361 9.00781 2.01151 8.95709 2.07489 8.89318L4.99989 5.96137L7.92489 8.89318C7.98828 8.95709 8.06369 9.00781 8.14677 9.04243C8.22986 9.07704 8.31897 9.09486 8.40898 9.09486C8.49899 9.09486 8.58811 9.07704 8.67119 9.04243C8.75428 9.00781 8.82969 8.95709 8.89307 8.89318C8.95698 8.8298 9.0077 8.75439 9.04232 8.6713C9.07693 8.58822 9.09475 8.4991 9.09475 8.40909C9.09475 8.31908 9.07693 8.22997 9.04232 8.14688C9.0077 8.0638 8.95698 7.98839 8.89307 7.925L5.96126 5Z"
+                  />
+                }
+              />
+            }
+          ></Header>
+          <div className={styles.sportCategoriesList}>
+            <SportCard
+              title="Football (Soccer)"
+              subTitle="Start a football match with locals"
+              image={football}
+              callback={() => setSportCategory('football')}
+            ></SportCard>
 
-          <SportCard
-            title="Basketball"
-            subTitle="Organize a basketball game"
-            image={basketball}
-            callback={() => setSportCategory('basketball')}
-          ></SportCard>
+            <SportCard
+              title="Basketball"
+              subTitle="Organize a basketball game"
+              image={basketball}
+              callback={() => setSportCategory('basketball')}
+            ></SportCard>
 
-          <SportCard
-            title="Beach Volleyball"
-            subTitle="Step on the warm sand for a round of volleyball"
-            image={beachvolleyball}
-            callback={() => setSportCategory('beachvolleyball')}
-          ></SportCard>
+            <SportCard
+              title="Beach Volleyball"
+              subTitle="Step on the warm sand for a round of volleyball"
+              image={beachvolleyball}
+              callback={() => setSportCategory('beachvolleyball')}
+            ></SportCard>
 
-          <SportCard
-            title="Tennis"
-            subTitle="Challange yourself with new tennis opponents"
-            image={tennis}
-            callback={() => setSportCategory('tennis')}
-          ></SportCard>
+            <SportCard
+              title="Tennis"
+              subTitle="Challange yourself with new tennis opponents"
+              image={tennis}
+              callback={() => setSportCategory('tennis')}
+            ></SportCard>
 
-          <SportCard
-            title="Volleyball"
-            subTitle="Level up your volley skills?"
-            image={volleyball}
-            callback={() => setSportCategory('volleyball')}
-          ></SportCard>
+            <SportCard
+              title="Volleyball"
+              subTitle="Level up your volley skills?"
+              image={volleyball}
+              callback={() => setSportCategory('volleyball')}
+            ></SportCard>
 
-          <SportCard
-            title="Ultimate Frisbee"
-            subTitle="I have no ideas for cool prompts?"
-            image={frisbee}
-            callback={() => setSportCategory('frisbee')}
-          ></SportCard>
+            <SportCard
+              title="Ultimate Frisbee"
+              subTitle="I have no ideas for cool prompts?"
+              image={frisbee}
+              callback={() => setSportCategory('frisbee')}
+            ></SportCard>
 
-          <div className={styles.bottomMargin}></div>
-        </div>
-        <Footer
-          progress={25}
-          leftSide={<p onClick={() => Router.back()}>Back</p>}
-          rightButton={<Button variant="primary" callback={goToNext} text="Next"></Button>}
-        ></Footer>
-      </div>
-    );
-  if (step == 1)
-    return (
-      <>
-        <Header
-          title="Give us some general information"
-          leftButton={
-            <HeaderButton
-              viewBox="0 0 10 10"
-              callback={() => Router.back()}
-              icon={
-                <path
-                  xmlns="http://www.w3.org/2000/svg"
-                  d="M5.96126 5L8.89307 2.075C9.02146 1.94661 9.09359 1.77248 9.09359 1.59091C9.09359 1.40934 9.02146 1.23521 8.89307 1.10682C8.76468 0.978432 8.59055 0.906303 8.40898 0.906303C8.22741 0.906303 8.05328 0.978432 7.92489 1.10682L4.99989 4.03864L2.07489 1.10682C1.9465 0.978432 1.77237 0.906303 1.5908 0.906303C1.40923 0.906303 1.2351 0.978432 1.10671 1.10682C0.978321 1.23521 0.906193 1.40934 0.906193 1.59091C0.906193 1.77248 0.978321 1.94661 1.10671 2.075L4.03853 5L1.10671 7.925C1.0428 7.98839 0.992081 8.0638 0.957466 8.14688C0.922851 8.22997 0.905029 8.31908 0.905029 8.40909C0.905029 8.4991 0.922851 8.58822 0.957466 8.6713C0.992081 8.75439 1.0428 8.8298 1.10671 8.89318C1.17009 8.95709 1.2455 9.00781 1.32859 9.04243C1.41168 9.07704 1.50079 9.09486 1.5908 9.09486C1.68081 9.09486 1.76993 9.07704 1.85301 9.04243C1.9361 9.00781 2.01151 8.95709 2.07489 8.89318L4.99989 5.96137L7.92489 8.89318C7.98828 8.95709 8.06369 9.00781 8.14677 9.04243C8.22986 9.07704 8.31897 9.09486 8.40898 9.09486C8.49899 9.09486 8.58811 9.07704 8.67119 9.04243C8.75428 9.00781 8.82969 8.95709 8.89307 8.89318C8.95698 8.8298 9.0077 8.75439 9.04232 8.6713C9.07693 8.58822 9.09475 8.4991 9.09475 8.40909C9.09475 8.31908 9.07693 8.22997 9.04232 8.14688C9.0077 8.0638 8.95698 7.98839 8.89307 7.925L5.96126 5Z"
-                />
-              }
-            />
-          }
-        ></Header>
-
-        <form className={styles.generalInfoForm}>
-          <div className={styles.inputGroup}>
-            <label className={styles.label}>Title</label>
-            <input
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Give your MatchUp a cool name"
-              className={styles.input}
-              style={{ borderColor: '#DDDDDD' }}
-            ></input>
+            <div className={styles.bottomMargin}></div>
           </div>
+          <Footer
+            progress={25}
+            leftSide={<p onClick={() => Router.back()}>Back</p>}
+            rightButton={<Button variant="primary" callback={goToNext} text="Next"></Button>}
+          ></Footer>
+        </>
+      ) : step == 1 ? (
+        <>
+          <Header
+            title="Give us some general information"
+            leftButton={
+              <HeaderButton
+                viewBox="0 0 10 10"
+                callback={() => Router.back()}
+                icon={
+                  <path
+                    xmlns="http://www.w3.org/2000/svg"
+                    d="M5.96126 5L8.89307 2.075C9.02146 1.94661 9.09359 1.77248 9.09359 1.59091C9.09359 1.40934 9.02146 1.23521 8.89307 1.10682C8.76468 0.978432 8.59055 0.906303 8.40898 0.906303C8.22741 0.906303 8.05328 0.978432 7.92489 1.10682L4.99989 4.03864L2.07489 1.10682C1.9465 0.978432 1.77237 0.906303 1.5908 0.906303C1.40923 0.906303 1.2351 0.978432 1.10671 1.10682C0.978321 1.23521 0.906193 1.40934 0.906193 1.59091C0.906193 1.77248 0.978321 1.94661 1.10671 2.075L4.03853 5L1.10671 7.925C1.0428 7.98839 0.992081 8.0638 0.957466 8.14688C0.922851 8.22997 0.905029 8.31908 0.905029 8.40909C0.905029 8.4991 0.922851 8.58822 0.957466 8.6713C0.992081 8.75439 1.0428 8.8298 1.10671 8.89318C1.17009 8.95709 1.2455 9.00781 1.32859 9.04243C1.41168 9.07704 1.50079 9.09486 1.5908 9.09486C1.68081 9.09486 1.76993 9.07704 1.85301 9.04243C1.9361 9.00781 2.01151 8.95709 2.07489 8.89318L4.99989 5.96137L7.92489 8.89318C7.98828 8.95709 8.06369 9.00781 8.14677 9.04243C8.22986 9.07704 8.31897 9.09486 8.40898 9.09486C8.49899 9.09486 8.58811 9.07704 8.67119 9.04243C8.75428 9.00781 8.82969 8.95709 8.89307 8.89318C8.95698 8.8298 9.0077 8.75439 9.04232 8.6713C9.07693 8.58822 9.09475 8.4991 9.09475 8.40909C9.09475 8.31908 9.07693 8.22997 9.04232 8.14688C9.0077 8.0638 8.95698 7.98839 8.89307 7.925L5.96126 5Z"
+                  />
+                }
+              />
+            }
+          ></Header>
 
-          <div className={styles.inputGroup}>
-            <label className={styles.label}>Time</label>
-            <input
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-              placeholder="Choose a day and time to MatchUp"
-              className={styles.input}
-              type="datetime-local"
-              style={{ borderColor: '#DDDDDD' }}
-            ></input>
-          </div>
+          <form className={styles.generalInfoForm}>
+            <div className={styles.inputGroup}>
+              <label className={styles.label} style={{ color: colors.text[100] }}>
+                Title
+              </label>
+              <input
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Give your MatchUp a cool name"
+                className={styles.input}
+                style={{ borderColor: darkMode ? colors.background[60] : '#DDDDDD', color: colors.text[60] }}
+              ></input>
+            </div>
 
-          <div className={styles.inputGroup}>
-            <label className={styles.label}>Location</label>
-            <input
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-              placeholder="Where do you want to meet?"
-              className={styles.input}
-              style={{ borderColor: '#DDDDDD' }}
-            ></input>
-          </div>
-        </form>
+            <div className={styles.inputGroup}>
+              <label className={styles.label} style={{ color: colors.text[100] }}>
+                Time
+              </label>
+              <input
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                placeholder="Choose a day and time to MatchUp"
+                className={styles.input}
+                type="datetime-local"
+                style={{ borderColor: darkMode ? colors.background[60] : '#DDDDDD', color: colors.text[60] }}
+              ></input>
+            </div>
 
-        {/* <div>Map going here</div>
-        <div>Switch going here</div> */}
+            <div className={styles.inputGroup}>
+              <label className={styles.label} style={{ color: colors.text[100] }}>
+                Location
+              </label>
+              <input
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                placeholder="Where do you want to meet?"
+                className={styles.input}
+                style={{ borderColor: darkMode ? colors.background[60] : '#DDDDDD', color: colors.text[60] }}
+              ></input>
+            </div>
+          </form>
 
-        <Footer
-          progress={50}
-          leftSide={<p onClick={() => goBack()}>Back</p>}
-          rightButton={<Button variant="primary" callback={goToNext} text="Next"></Button>}
-        ></Footer>
-      </>
-    );
+          {/* <div>Map going here</div>
+            <div>Switch going here</div> */}
+
+          <Footer
+            progress={50}
+            leftSide={<p onClick={() => goBack()}>Back</p>}
+            rightButton={<Button variant="primary" callback={goToNext} text="Next"></Button>}
+          ></Footer>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
 export default OrganizePage;
