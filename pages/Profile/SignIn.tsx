69d6f001b3ebe92bfebd47a387de2a08d0ed182d
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-29 20:42:12 +0200

    start working on signup page

--- pages/Profile/SignIn.tsx
+++ pages/Profile/SignIn.tsx
@@ -1,20 +1,76 @@
-import { useState, useEffect, FC } from 'react'
+import { useState, useEffect, FC } from 'react';
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Auth } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
+import { useRouter } from 'next/router';
+import { NextPage } from 'next';
+import background from '../../public/background.png';
+import styles from './styles/SignIn.module.scss';
 
-function SignIn<FC>() {
-  const { user, signOut } = useAuthenticator((context) => [context.user]);
+const SignInPage: NextPage = () => {
+  const router = useRouter();
+  const { authStatus, user } = useAuthenticator((context) => [context.authStatus, context.user]);
+
+  /* LOG IN */
+  const [loginError, setLoginError] = useState<string>();
+  async function handleLogin(e) {
+    e.preventDefault();
+    const email = e.target['login-email'].value;
+    const password = e.target['login-pw'].value;
+
+    try {
+      await Auth.signIn(email, password);
+      setLoginError('');
+      router.push('/');
+    } catch (err) {
+      setLoginError('Wrong credentials. Please try again.');
+    }
+  }
+
+  /* REGISTRATION */
+  const [showRegistration, setShowRegistration] = useState(true);
+  const [registrationError, setRegistrationError] = useState<string>();
+  async function handleRegistration(e) {
+    e.preventDefault();
+    const username = e.target['registration-email'].value;
+    const password = e.target['registration-pw'].value;
+    const given_name = e.target['given-name'].value;
+    const family_name = e.target['family-name'].value;
+    const confirmPassword = e.target['registration-pw-confirm'].value;
+
+    if (password === confirmPassword) {
+      try {
+        await Auth.signUp({
+          username,
+          password,
+          attributes: { email: username, given_name, family_name },
+        });
+        setRegistrationError('');
+        router.push('/');
+      } catch (err) {
+        setRegistrationError('User could not be registred');
+      }
+    } else {
+      setRegistrationError('Passwords are not matching');
+    }
+  }
+
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
+    <div className={styles.page} style={{ backgroundImage: `url(${background.src})` }}>
+      <div style={{ backgroundColor: 'white', borderRadius: '100%', width: '150px', height: '150px' }} />
+      {showRegistration ? (
+        <form onSubmit={handleRegistration}>
+          <input
+            className={styles.input}
+            type="email"
+            name="registration-email"
+            placeholder="Your email address"
+          ></input>
+        </form>
+      ) : (
+        <form onSubmit={handleLogin}></form>
       )}
-    </Authenticator>
+    </div>
   );
-}
-
-export default SignIn;
+};
+export default SignInPage;
