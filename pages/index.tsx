644a74d2657af8dc52b0534f52371c0f46adfed8
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-06-29 15:18:58 +0200

    started testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next';
 import { useTheme } from '../contexts/Theme';
 import Navigation from '../components/misc/Navigation';
+import { addUserToMatchUp } from '../utils/Mutation/addUserToMatchUp.util';
+import { }
 
 const Home: NextPage = () => {
+
+  
+
   const { toggleDarkMode } = useTheme();
   return (
     <>
