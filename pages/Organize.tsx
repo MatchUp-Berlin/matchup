3c034787758c9d3e3706e920ace00413fb048236
l-stend <stendardo.lu@gmail.com>
2022-06-29 08:12:13 +0200

    fix: upload image presentable

--- pages/Organize.tsx
+++ pages/Organize.tsx
@@ -411,26 +411,35 @@ const OrganizePage: NextPage = () => {
           <h4 className={styles.inputSectionTitle} style={{ marginTop: '0' }}>
             Update Title Image
           </h4>
-          <input
-            className={styles.imageInput}
-            type='file'
-            onClick={() => console.log('input file')}
-            style={{
-              borderColor: darkMode ? colors.background[60] : '#DDDDDD',
-            }}
-          />
-          <h4 className={styles.inputSectionTitle} style={{ marginTop: '0' }}>
-            Description
-          </h4>
-          <h4 className={styles.inputSectionTitle} style={{ marginTop: '0' }}>
-            Description
-          </h4>
-          <h4 className={styles.inputSectionTitle} style={{ marginTop: '0' }}>
-            Description
-          </h4>
-          <h4 className={styles.inputSectionTitle} style={{ marginTop: '0' }}>
-            Description
-          </h4>
+
+          <div className={styles.uploadImageWrapper}>
+            <button
+              className={styles.inputImageBtn}
+              type='button'
+              style={{
+                borderColor: darkMode ? colors.background[60] : '#DDDDDD',
+                color: colors.text[60],
+              }}
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='20'
+                height='20'
+                fill='currentColor'
+                className='bi bi-upload'
+                viewBox='0 0 16 16'
+                style={{
+                  color: colors.text[60],
+                }}
+              >
+                <path d='M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z' />
+                <path d='M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z' />
+              </svg>
+              {'    '}
+              How do you like it, little white boy?
+              <input className={styles.imageInput} type='file' />
+            </button>
+          </div>
 
           <Footer
             progress={75}
