20d394c863880ce4a3583595c5854d0849e5ed1f
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-29 15:51:04 +0200

    fixed avatar, and userspreview

--- components/cards/ParticipantsPreview.Card.tsx
+++ components/cards/ParticipantsPreview.Card.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { User } from '../../utils/types/User.Type';
 import { Avatar } from '../misc';
+import SmallButton from '../misc/SmallButton';
 import styles from './styles/ParticipantsPreview.Card.module.scss';
 
 export interface IParticipantsPreviewCardProps {
@@ -9,10 +10,13 @@ export interface IParticipantsPreviewCardProps {
 
 const ParticipantsPreviewCard: React.FunctionComponent<IParticipantsPreviewCardProps> = ({ users }) => {
   return (
-    <div className={styles.avatarPreview}>
-      {users.slice(0, 6).map((user) => (
-        <Avatar key={user.id} size={'small'} image={user.profileImage} />
-      ))}
+    <div className={styles.wrapper}>
+      <div className={styles.avatarPreview}>
+        {users.slice(0, 6).map((user) => (
+          <Avatar key={user.id} size={'small'} image={user.profileImage} />
+        ))}
+      </div>
+      <SmallButton callback={() => console.log('clicked')} />
     </div>
   );
 };
