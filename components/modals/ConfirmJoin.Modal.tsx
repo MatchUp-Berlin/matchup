a5b2020d1acf3a4955ea442ec11618dab939310b
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-06-30 21:33:26 +0200

    add styling

--- components/modals/ConfirmJoin.Modal.tsx
+++ components/modals/ConfirmJoin.Modal.tsx
@@ -3,6 +3,7 @@ import { useTheme } from '../../contexts/Theme';
 import { MatchUp } from '../../utils/types/MatchUp.Type';
 import styles from './styles/ConfirmJoin.Modal.module.scss';
 import Button from '../misc/Button';
+import moment from 'moment';
 
 export interface IConfirmJoinModalProps {
   matchUp: MatchUp;
@@ -24,18 +25,34 @@ const ConfirmJoinModal: React.FunctionComponent<IConfirmJoinModalProps> = (
         style={{ backgroundColor: 'white' }}
         className={styles.modalContainer}
       >
-        <h2>Join {props.matchUp.title}</h2>
-        <p>{props.matchUp.location}</p>
-        <p>{props.matchUp.date}</p>
-        <p>
-          In order to join this MatchUp you have to commit a deposit of 5€. You
-          will get the deposit back once the organizer confirms your attendance.
-          In case you cannot make it, you can cancel the MatchUp up to 24 hours
-          in advance. If you fail to do so, your deposit will not be returned to
-          you. Instead it will be donated to a local charity in your city.{' '}
+        <div className={styles.modalInfo}>
+          <h2 className={styles.modalHeading}>Join {props.matchUp.title}</h2>
+          {/* <p>{props.matchUp.location}</p>
+          <p>{moment(props.matchUp.date).format('H:m dddd')}</p> */}
+        </div>
+
+        <p className={styles.modalCommitTextPrimary}>
+          In order to join this MatchUp you have to commit a deposit of 5€.
         </p>
-        <p>Read more about our commitment policy</p>
-        <Button></Button>
+
+        <p className={styles.modalCommitTextSecondary}>
+          You will get the deposit back once the organizer confirms your
+          attendance. In case you cannot make it, you can cancel the MatchUp up
+          to 24 hours in advance. If you fail to do so, your deposit will not be
+          returned to you. Instead it will be donated to a local charity in your
+          city.
+        </p>
+
+        <a className={styles.modalPolicyLink}>
+          Read more about our commitment policy
+        </a>
+        <Button
+          className={styles.modalButton}
+          variant='primary'
+          callback={() => console.log('clicked')}
+          text='Commit'
+          disabled={false}
+        ></Button>
       </div>
     </div>
   );
