69c62b4feb7ca906cd2f8f222b85a267352e3c85
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-06-30 19:41:30 +0200

    basic setup confirmJoin modal

--- components/modals/ConfirmJoin.Modal.tsx
+++ components/modals/ConfirmJoin.Modal.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useTheme } from '../../contexts/Theme';
+import { MatchUp } from '../../utils/types/MatchUp.Type';
+import styles from './styles/ConfirmJoin.Modal.module.scss';
 
-export interface IConfirmJoinModalProps {}
+export interface IConfirmJoinModalProps {
+  matchUp: MatchUp;
+}
 
-const ConfirmJoinModal: React.FunctionComponent<IConfirmJoinModalProps> = (props) => {
-  return <></>;
+// a4358bf3-9ef1-4c06-b023-f87a5e39cbf6
+
+const ConfirmJoinModal: React.FunctionComponent<IConfirmJoinModalProps> = (
+  props
+) => {
+  const { colors, shadows } = useTheme();
+
+  return <div className={styles.container}>Hallo</div>;
 };
 
 export default ConfirmJoinModal;
