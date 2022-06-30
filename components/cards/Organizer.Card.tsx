bc5c441e05f61be8226aa0aecb23257637980198
Samuel Fanhais <samuelfanhais@gmail.com>
2022-06-30 10:52:54 +0200

    feat: edit participant card and create organizer card

--- components/cards/Organizer.Card.tsx
+++ components/cards/Organizer.Card.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { User } from '../../utils/types/User.Type';
+import { Avatar } from '../misc';
+import { useTheme } from '../../contexts/Theme';
+import styles from './styles/Organizer.Card.module.scss';
 
-export interface IOrganizerCardProps {}
+export interface IOrganizerCardProps {
+  user: User;
+}
 
-const OrganizerCard: React.FunctionComponent<IOrganizerCardProps> = (props) => {
-  return <></>;
+const OrganizerCard: React.FunctionComponent<IOrganizerCardProps> = ({ user }) => {
+  const { profileImage, givenName, familyName, signups } = user;
+  const { colors } = useTheme();
+  return (
+    <>
+      <div className={styles.organizerCardWrapper}>
+        <div className="infoContainer">
+          <h3 style={{ color: colors.text[100] }}>{`${givenName} ${familyName}`}</h3>
+          <div className={styles.subtext}>
+            <p style={{ color: colors.text[60] }}>{`Participated in ${signups.length} MatchUps`}</p>
+            <p style={{ color: colors.text[60] }}>Organizer</p>
+          </div>
+        </div>
+        <div className={styles.avatarContainer}>
+          <Avatar size={'medium'} image={profileImage} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default OrganizerCard;
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next';
 import UpdatesPreviewCard from '../components/cards/UpdatesPreview.Card';
+import ParticipantCard from '../components/cards/Participant.Card';
+import OrganizerCard from '../components/cards/Organizer.Card';
+import { usersArray } from './../mockData/mockUsersArray';
 
 const organizerId = '67523476';
 
@@ -70,7 +73,9 @@ const updates = [
 const Home: NextPage = () => {
   return (
     <>
-      <UpdatesPreviewCard updates={updates} organizerId={organizerId} />
+      {/* <UpdatesPreviewCard updates={updates} organizerId={organizerId} /> */}
+      <ParticipantCard user={usersArray[0]} />
+      <OrganizerCard user={usersArray[3]} />
     </>
   );
 };
