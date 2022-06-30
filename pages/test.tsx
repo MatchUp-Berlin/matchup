badad8dfb35a4b97a55f9777a4447063648a718a
Samuel Fanhais <samuelfanhais@gmail.com>
2022-06-30 19:14:14 +0200

    Feat: update message card (#49)
    * feat: add update message card
    
    * [CodeFactor] Apply fixes
    
    Co-authored-by: codefactor-io <support@codefactor.io>

--- pages/test.tsx
+++ pages/test.tsx
@@ -3,13 +3,18 @@ import UpdatesPreviewCard from '../components/cards/UpdatesPreview.Card';
 import ParticipantCard from '../components/cards/Participant.Card';
 import OrganizerCard from '../components/cards/Organizer.Card';
 import { usersArray } from './../mockData/mockUsersArray';
+import UpdatesMessageCard from '../components/cards/UpdatesMessage.Card';
+import MatchUpDetail from './MatchUps/[MatchUpId]';
+import { mockUpdate } from './../mockData/mockUpdate';
 
 const Home: NextPage = () => {
   return (
     <>
       {/* <UpdatesPreviewCard updates={updates} organizerId={organizerId} /> */}
-      <ParticipantCard user={usersArray[0]} />
-      <OrganizerCard user={usersArray[3]} />
+      {/* <ParticipantCard user={usersArray[0]} /> */}
+      {/* <OrganizerCard user={usersArray[3]} /> */}
+      {/* <UpdatesMessageCard update={mockUpdate} /> */}
+      <MatchUpDetail />
     </>
   );
 };
