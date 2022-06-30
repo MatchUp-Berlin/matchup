a761eee186dd3d51b4c2c500740807be459a36a9
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-30 10:21:31 +0200

    init

---
+++ pages/MyMatchUps/[userId].tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next';
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useRouter } from 'next/router';
+import Navigation from '../../components/misc/Navigation';
+import { useTheme } from '../../contexts/Theme';
+import { useState } from 'react';
+
+import styles from './styles/MyMatchUps.module.scss';
+
+const YourMatchUpsPage: NextPage = () => {
+  const { colors } = useTheme();
+  const router = useRouter();
+  const { route } = useAuthenticator((context) => [context.route]);
+
+  const [showOrganizing, setShowOrganizing] = useState<boolean>(false);
+
+  if (route !== 'authenticated') {
+    router.push('/SignIn');
+    return <></>;
+  } else {
+    return (
+      <>
+        <div className={styles.wrapper}></div>
+        <Navigation></Navigation>
+      </>
+    );
+  }
+};
+export default YourMatchUpsPage;
