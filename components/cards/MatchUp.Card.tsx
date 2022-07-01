066fe6e274e313a71e29ffb325ff511460de538e
l-stend <stendardo.lu@gmail.com>
2022-07-01 11:22:27 +0200

    fix: MatchUpCard seems ok with upadted matchUp type

--- components/cards/MatchUp.Card.tsx
+++ components/cards/MatchUp.Card.tsx
@@ -11,23 +11,49 @@ import { TSkillLevels, TSportCategories } from '../../utils/types/MatchUp.Type';
 import getSportIcon from '../../utils/getSportIcon';
 import getDefaultImage from '../../utils/getDefaultImage';
 import Link from 'next/link';
+import { Update } from '../../utils/types/Update.Type';
+import { User } from '../../utils/types/User.Type';
+import { WatchList } from '../../utils/types/WatchList.Type';
+import { SignUpsReturn } from '../../utils/types/SignUp.Type';
 
 //TODO: Adjust to actual types!!
 
 export interface IMatchUpCardProps {
-  id: string;
   variant: 'small' | 'large';
-  timestamp: string;
+  id?: string;
   title: string;
-  slots?: number;
-  participating: number;
   location: string;
-  sport: TSportCategories;
-  skill?: TSkillLevels;
-  imageUrl?: string;
-  paid?: boolean;
-  price: number;
-  rented?: boolean;
+  coordinates: number[];
+  signups: SignUpsReturn;
+  organizerId: string;
+  organizer?: User;
+  sportCategory: TSportCategories;
+  skillLevel: TSkillLevels;
+  totalCost: number;
+  reservedCourt: boolean;
+  attendanceMin: number;
+  attendanceMax: number;
+  cancelled: boolean;
+  description: string;
+  image: string;
+  date: string;
+  currency: string;
+  updates: Update[];
+  watchList: WatchList[];
+  indoor: boolean;
+  // id: string;
+  // variant: 'small' | 'large';
+  // timestamp: string;
+  // title: string;
+  // slots?: number;
+  // participating: number;
+  // location: string;
+  // sport: TSportCategories;
+  // skill?: TSkillLevels;
+  // imageUrl?: string;
+  // paid?: boolean;
+  // price: number;
+  // rented?: boolean;
 }
 
 const MatchUpCard: React.FunctionComponent<IMatchUpCardProps> = (props) => {
@@ -44,36 +70,64 @@ const MatchUpCard: React.FunctionComponent<IMatchUpCardProps> = (props) => {
       >
         <div className={styles.imageWrapper}>
           <Image
-            src={props.imageUrl || getDefaultImage(props.sport)}
+            src={props.image || getDefaultImage(props.sportCategory)}
             alt={props.title}
-            layout="fill"
-            objectFit="cover"
+            layout='fill'
+            objectFit='cover'
           ></Image>
         </div>
 
         <div className={styles.infoWrapper}>
           <div
             className={styles.info}
-            style={props.variant == 'small' ? { justifyContent: 'space-around', height: '100%' } : {}}
+            style={
+              props.variant == 'small'
+                ? { justifyContent: 'space-around', height: '100%' }
+                : {}
+            }
           >
             <div className={styles.title} style={{ color: colors.text[100] }}>
-              <Image width={'20px'} height={'20px'} src={getSportIcon(props.sport)} alt={props.sport}></Image>
-              <p className="highlight-2">{props.title}</p>
+              <Image
+                width={'20px'}
+                height={'20px'}
+                src={getSportIcon(props.sportCategory)}
+                alt={props.sportCategory}
+              ></Image>
+              <p className='highlight-2'>{props.title}</p>
             </div>
             <div className={styles.details}>
               <div className={styles.detail}>
-                <Image width={'10px'} height={'10px'} src={clock} alt="taking place on"></Image>
-                <p style={{ color: colors.text[60] }}>{moment(props.timestamp).format('H:m dddd')}</p>
+                <Image
+                  width={'10px'}
+                  height={'10px'}
+                  src={clock}
+                  alt='taking place on'
+                ></Image>
+                <p style={{ color: colors.text[60] }}>
+                  {moment(props.date).format('H:m dddd')}
+                </p>
               </div>
               <div className={styles.detail}>
-                <Image width={'10px'} height={'10px'} src={pin} alt="taking place at"></Image>
+                <Image
+                  width={'10px'}
+                  height={'10px'}
+                  src={pin}
+                  alt='taking place at'
+                ></Image>
                 <p style={{ color: colors.text[60] }}>{props.location}</p>
               </div>
 
               {props.variant == 'large' && (
                 <div className={styles.detail}>
-                  <Image width={'10px'} height={'10px'} src={euro} alt="costs"></Image>
-                  <p style={{ color: colors.text[60] }}>{props.price > 0 ? props.price + '.00' : 'Free'}</p>
+                  <Image
+                    width={'10px'}
+                    height={'10px'}
+                    src={euro}
+                    alt='costs'
+                  ></Image>
+                  <p style={{ color: colors.text[60] }}>
+                    {props.totalCost > 0 ? props.totalCost + '.00' : 'Free'}
+                  </p>
                 </div>
               )}
             </div>
@@ -83,26 +137,40 @@ const MatchUpCard: React.FunctionComponent<IMatchUpCardProps> = (props) => {
             <div className={styles.pills}>
               <div
                 className={styles.pill}
-                style={{ backgroundColor: colors.background[100], boxShadow: shadows.small }}
+                style={{
+                  backgroundColor: colors.background[100],
+                  boxShadow: shadows.small,
+                }}
               >
-                <p style={{ color: colors.text[100] }}>{props.skill}</p>
+                <p style={{ color: colors.text[100] }}>{props.skillLevel}</p>
               </div>
-              {props.rented && (
+              {props.reservedCourt && (
                 <div
                   className={styles.pill}
-                  style={{ backgroundColor: colors.background[100], boxShadow: shadows.small }}
+                  style={{
+                    backgroundColor: colors.background[100],
+                    boxShadow: shadows.small,
+                  }}
                 >
                   <p style={{ color: colors.text[100] }}>Rented</p>
                 </div>
               )}
-              {props.variant == 'large' && props.slots && props.slots - props.participating && (
-                <div
-                  className={styles.pill}
-                  style={{ backgroundColor: colors.background[100], boxShadow: shadows.small }}
-                >
-                  <p style={{ color: colors.primary[100] }}>{props.slots - props.participating} Spots left</p>
-                </div>
-              )}
+              {props.variant == 'large' &&
+                props.attendanceMin &&
+                props.attendanceMin - props.signups.items.length && (
+                  <div
+                    className={styles.pill}
+                    style={{
+                      backgroundColor: colors.background[100],
+                      boxShadow: shadows.small,
+                    }}
+                  >
+                    <p style={{ color: colors.primary[100] }}>
+                      {props.attendanceMin - props.signups.items.length} Spots
+                      left
+                    </p>
+                  </div>
+                )}
             </div>
           )}
         </div>
