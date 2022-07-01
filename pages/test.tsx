b561923ec1547e103cea44424511218b79e3e59c
Maxim Buz <85324130+MaximBuz@users.noreply.github.com>
2022-07-01 11:21:48 +0200

    feat: Participants modal (#53)
    * Animating opening participants modal

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,22 +1,8 @@
 import type { NextPage } from 'next';
-import UpdatesPreviewCard from '../components/cards/UpdatesPreview.Card';
-import ParticipantCard from '../components/cards/Participant.Card';
-import OrganizerCard from '../components/cards/Organizer.Card';
-import { usersArray } from './../mockData/mockUsersArray';
-import UpdatesMessageCard from '../components/cards/UpdatesMessage.Card';
-import MatchUpDetail from './MatchUps/[MatchUpId]';
-import { mockUpdate } from './../mockData/mockUpdate';
 
 const Home: NextPage = () => {
-  return (
-    <>
-      {/* <UpdatesPreviewCard updates={updates} organizerId={organizerId} /> */}
-      {/* <ParticipantCard user={usersArray[0]} /> */}
-      {/* <OrganizerCard user={usersArray[3]} /> */}
-      {/* <UpdatesMessageCard update={mockUpdate} /> */}
-      <MatchUpDetail />
-    </>
-  );
+
+  return <></>;
 };
 
 export default Home;
