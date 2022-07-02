19302cb6df22883c3edc80673838eba77b96eae6
Maxim Buz <85324130+MaximBuz@users.noreply.github.com>
2022-07-02 13:53:30 +0200

    fixed getmatchupbyid (#71)

--- utils/Query/getMatchUpById.util.ts
+++ utils/Query/getMatchUpById.util.ts
@@ -7,25 +7,26 @@ import { MatchUp } from '../types/MatchUp.Type';
 
 export async function getMatchUpById(id: string): Promise<MatchUp> {
   try {
-      const matchUpData = await API.graphql({
+    const matchUpData = await API.graphql({
       query: getMatchUp,
       variables: { id: id },
       // authMode: 'AMAZON_COGNITO_USER_POOLS'
     });
-      const retrievedMatchUpData = matchUpData.data.getMatchUp;
 
-      retrievedMatchUpData.image = await Storage.get(retrievedMatchUpData.id);
+    const retrievedMatchUpData = matchUpData.data.getMatchUp;
 
-      retrievedMatchUpData.items.forEach((element) => {
-        element.address = JSON.parse(element.address);
-    });
+    /* Fill with Image data */
+    retrievedMatchUpData.image = await Storage.get(retrievedMatchUpData.id);
 
-      return retrievedMatchUpData;
+    /* Fill with Address data */
+    retrievedMatchUpData.address = JSON.parse(retrievedMatchUpData.address);
+
+    return retrievedMatchUpData;
   } catch (error) {
+    console.log(error);
     throw error;
   }
 }
-
 /*
 EXAMPLE RESPONSE
 
