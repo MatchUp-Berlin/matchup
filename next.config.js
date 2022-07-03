055dfbb9cd98f0dd0a54286ee77f3b964b664708
Mitchell <mitchellryansmith@gmail.com>
2022-07-03 20:30:12 +0200

    reset up env vars

--- next.config.js
+++ next.config.js
@@ -19,4 +19,18 @@ module.exports = withPWA({
     register: true,
     skipWaiting: true,
   },
+  env: {
+    aws_project_region: process.env.aws_project_region,
+    aws_appsync_graphqlEndpoint: process.env.aws_appsync_graphqlEndpoint,
+    aws_appsync_region: process.env.aws_appsync_region,
+    aws_appsync_authenticationType: 'API_KEY',
+    aws_appsync_apiKey: process.env.aws_appsync_apiKey,
+    aws_cognito_identity_pool_id: process.env.aws_cognito_identity_pool_id,
+    aws_cognito_region: process.env.aws_cognito_region,
+    aws_user_pools_id: process.env.aws_user_pools_id,
+    aws_user_pools_web_client_id: process.env.aws_user_pools_web_client_id,
+    aws_user_files_s3_bucket: process.env.aws_user_files_s3_bucket,
+    aws_user_files_s3_bucket_region:
+      process.env.aws_user_files_s3_bucket_region,
+  },
 });
