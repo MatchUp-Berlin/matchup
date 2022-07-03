785b59ff42c69935cff3b916a066fda188e167d7
Mitchell <mitchellryansmith@gmail.com>
2022-07-03 16:55:26 +0200

    pwa testing

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = {
+module.exports = withPWA({
   typescript: { ignoreBuildErrors: true },
   nextConfig,
   images: {
@@ -12,4 +14,4 @@ module.exports = {
       'matchup-storage-b0c9ab6f112810-dev.s3.us-east-1.amazonaws.com',
     ],
   },
-};
+});
