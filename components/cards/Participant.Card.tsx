dab752344190a7f5ccf1e76e92aae4cc95849e50
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-07-04 13:14:33 +0200

    setup color structure

--- components/cards/Participant.Card.tsx
+++ components/cards/Participant.Card.tsx
@@ -4,15 +4,19 @@ import { Avatar } from '../misc';
 import { useTheme } from '../../contexts/Theme';
 import styles from './styles/Participant.Card.module.scss';
 import avatar from '../../public/default-avatar.png';
-
+import { useAuth } from '../../contexts/Auth';
 
 export interface IParticipantCardProps {
   user: User;
 }
 
-const ParticipantCard: React.FunctionComponent<IParticipantCardProps> = ({ user }) => {
+const ParticipantCard: React.FunctionComponent<IParticipantCardProps> = ({
+  user,
+}) => {
   const { profileImage, givenName, familyName, signups } = user;
   const { colors, shadows } = useTheme();
+  const currentUser = useAuth();
+
   return (
     <article
       className={styles.participantCardWrapper}
@@ -22,13 +26,21 @@ const ParticipantCard: React.FunctionComponent<IParticipantCardProps> = ({ user 
       }}
     >
       <div className={styles.info}>
-        <p className="highlight-1" style={{ color: colors.text[100] }}>{`${givenName} ${familyName}`}</p>
+        <p
+          className='highlight-1'
+          style={{ color: colors.text[100] }}
+        >{`${givenName} ${familyName}`}</p>
         {/* <p style={{ color: colors.text[80] }}>{`Participated in ${signups.length} MatchUps`}</p> */}
         <p style={{ color: colors.text[60] }}>Speaks: </p>
       </div>
-      
+
       <div className={styles.avatar}>
-        <Avatar size={'medium'} image={profileImage || avatar} />
+        <Avatar
+          confirmedAttendance={true}
+          highlight={true}
+          size={'medium'}
+          image={profileImage || avatar}
+        />
       </div>
     </article>
   );
--- components/misc/Avatar.tsx
+++ components/misc/Avatar.tsx
@@ -3,15 +3,28 @@ import { useTheme } from '../../contexts/Theme';
 import ImageFallback from './ImageFallback';
 import getDefaultImage from '../../utils/getDefaultImage';
 import defaultAvatar from '../../public/default-avatar.png';
+import { confirmAttendance } from '../../utils/Mutation/confirmAttendance.util';
 
 export interface IAvatarProps {
   size: string;
   image: any;
+  highlight: boolean;
+  confirmedAttendance: boolean;
 }
 
-const Avatar: React.FunctionComponent<IAvatarProps> = ({ size, image }) => {
-  const { shadows } = useTheme();
+const Avatar: React.FunctionComponent<IAvatarProps> = ({
+  size,
+  image,
+  confirmedAttendance,
+  highlight,
+}) => {
+  const { colors, shadows } = useTheme();
   let avatarSize: string = '';
+
+  const border =
+    highlight && confirmedAttendance
+      ? `3px solid ${colors.primary[100]}`
+      : 'None';
 
   if (size === 'small') {
     avatarSize = '50px';
@@ -31,16 +44,17 @@ const Avatar: React.FunctionComponent<IAvatarProps> = ({ size, image }) => {
         width: avatarSize,
         height: avatarSize,
         overflow: 'hidden',
+        border: border,
       }}
     >
       <ImageFallback
         src={image}
         fallbackSrc={defaultAvatar.src}
-        layout="responsive"
-        objectFit="cover"
+        layout='responsive'
+        objectFit='cover'
         width={avatarSize}
         height={avatarSize}
-        alt="personally I would have replaced missing images with some Max-s mothers picture, but you now...pc people and stuff"
+        alt='personally I would have replaced missing images with some Max-s mothers picture, but you now...pc people and stuff'
       />
     </div>
   );
