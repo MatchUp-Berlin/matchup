545f4e0d3f5c99d0ae8f459429f5498e31e39d07
Mitchell Smith <mitchellryansmith@gmail.com>
2022-07-04 09:59:35 +0200

    feat: Deployment/PWA (#90)
    * feat: added map to matchup details

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = {
+module.exports = withPWA({
   typescript: { ignoreBuildErrors: true },
   nextConfig,
   images: {
@@ -12,4 +14,14 @@ module.exports = {
       'matchup-storage-b0c9ab6f112810-dev.s3.us-east-1.amazonaws.com',
     ],
   },
-};
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+  env: {
+    aws_appsync_graphqlEndpoint: process.env.aws_appsync_graphqlEndpoint,
+    aws_appsync_apiKey: process.env.aws_appsync_apiKey,
+    aws_user_files_s3_bucket: process.env.aws_user_files_s3_bucket,
+  },
+});
