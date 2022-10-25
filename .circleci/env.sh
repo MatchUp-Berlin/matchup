2c8759cf808e56350711267e54adb3002aa5f2ab
Mitchell <mitchellryansmith@gmail.com>
2022-10-25 15:33:09 -0500

    added quotes around env shell script

--- .circleci/env.sh
+++ .circleci/env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo aws_appsync_graphqlEndpoint=$aws_appsync_graphqlEndpoint >> ../.env.local
-echo aws_appsync_apiKey=$aws_appsync_apiKey >> ../.env.local
-echo aws_user_files_s3_bucket=$aws_user_files_s3_bucket >> ../.env.local
+touch ../.env.local
+echo aws_appsync_graphqlEndpoint='"'$aws_appsync_graphqlEndpoint'"' >> ../.env.local
+echo aws_appsync_apiKey='"'$aws_appsync_apiKey'"' >> ../.env.local
+echo aws_user_files_s3_bucket='"'$aws_user_files_s3_bucket'"' >> ../.env.local
