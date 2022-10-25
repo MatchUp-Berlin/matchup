4852c3b40920e27addae030a659782c406ddcf10
Mitchell Smith <mitchellryansmith@gmail.com>
2022-10-25 17:06:45 -0400

    Docker env vars (#158)
    * added env vars to docker file
    
    * added build args to circleci
    
    * testing image
    
    * fixed typo
    
    * added env shell script
    
    * added quotes around env shell script
    
    * removed build args
    
    * removed build args

---
+++ .circleci/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+touch ../.env.local
+echo aws_appsync_graphqlEndpoint='"'$aws_appsync_graphqlEndpoint'"' >> ../.env.local
+echo aws_appsync_apiKey='"'$aws_appsync_apiKey'"' >> ../.env.local
+echo aws_user_files_s3_bucket='"'$aws_user_files_s3_bucket'"' >> ../.env.local
